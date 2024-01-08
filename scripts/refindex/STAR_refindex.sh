5f6df2b50068927dee8ed6dc991ea9d1e62bf6d1
JosephLeger <95336622+JosephLeger@users.noreply.github.com>
2024-01-08 16:06:16 +0100

    Create STAR_refindex.sh
    Add STAR_refindex.sh

---
+++ scripts/refindex/STAR_refindex.sh
@@ -0,0 +1,61 @@
+#$ -V
+#$ -cwd
+#$ -S /bin/bash
+
+#!/bin/env bash
+
+################################################################################################################
+### HELP -------------------------------------------------------------------------------------------------------
+################################################################################################################
+
+# Text font variabes
+END='\033[0m'
+BOLD='\033[1m'
+UDL='\033[4m'
+
+Help()
+{
+echo -e "${BOLD}####### STAR_REFINDEX MANUAL #######${END}\n\n\
+${BOLD}SYNTHAX${END}\n\
+    sh STAR_refindex.sh <fasta_file> <gtf_file>\n\n\
+${BOLD}DESCRIPTION${END}\n\
+    \n\n\
+${BOLD}ARGUMENTS${END}\n\
+    ${BOLD}<fasta_file>${END}\n\
+        Path to FASTA file to use for making reference.\n
+        It can usually be downloaded from Ensembl genome browser.\n\n\
+    ${BOLD}<gtf_file>${END}\n\
+        Path to GTF file containing annotation that correspond to provided FASTA file.\n
+        It can usually be downloaded from Ensembl genome browser.\n\n\
+${BOLD}EXAMPLE USAGE${END}\n\
+    sh STAR_refindex.sh ${BOLD}../Genome/Mus_musculus.GRCm39.dna_sm.primary_assembly.fa ../Genome/Mus_musculus.GRCm39.108.gtf${END}\n"
+}
+
+################################################################################################################
+### ERRORS -----------------------------------------------------------------------------------------------------
+################################################################################################################
+
+if [ $# -eq 1 ] && [ $1 == "help" ]; then
+    Help
+    exit
+elif [ $# -ne 2 ]; then
+    # Error if inoccrect number of agruments is provided
+    echo 'Error synthax : please use following synthax'
+    echo '       sh STAR_refindex.sh <fasta_file> <gtf_file>'
+    exit
+elif [ ! -f "$1" ]; then
+    echo "Error : FASTA file not found. Please make sure provided pathway is correct."
+    exit
+elif [ ! -f "$2" ]; then
+    echo "Error : GTF file not found. Please make sure provided pathway is correct."
+    exit
+fi
+
+################################################################################################################
+### SCRIPT -----------------------------------------------------------------------------------------------------
+################################################################################################################
+
+module load star/2.7.10b
+
+echo -e "#$ -V \n#$ -cwd \n#$ -S /bin/bash \n\
+STAR --runMode genomeGenerate --genomeFastaFiles $1 --sjdbGTFfile $2 --runThreadN 16" | qsub -N STAR_RefIndex
